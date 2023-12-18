d67ccfc7970a6746567455a246213c97fefc61e7
funMango <138420832+funMango@users.noreply.github.com>
2023-12-18 10:13:23 +0900

    Project Start
    add project, gitignore

---
+++ UITestDemo/UITestDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UITestDemo
+//
+//  Created by 이민호 on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UITestDemo/UITestDemo/UITestDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UITestDemoApp.swift
+//  UITestDemo
+//
+//  Created by 이민호 on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct UITestDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
